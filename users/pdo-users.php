560d06e0d9cb442b8aeea4f1adfb961180aab62e
PinChi <artemismoon0804@gmail.com>
2025-02-25 16:24:55 +0800

    users:dosignUp/limit/whereLike

--- users/pdo-users.php
+++ users/pdo-users.php
@@ -1,20 +1,20 @@
 <?php
 require_once('../daiski/pdo_connect.php');
 
-$sql = "SELECT * FROM users WHERE valid=1";
-$stmtALL=$db_host->prepare($sql);
+$sqlALL = "SELECT * FROM users WHERE valid=1";
+$stmtALL = $db_host->prepare($sqlALL);
 $stmtALL->execute();
 $userCount = $stmtALL->rowCount();
 
 
 if (isset($_GET["q"])) {
     $q = $_GET["q"];
-    $sql = "SELECT * FROM users WHERE name LIKE'%$q%'";
+    $sql = "SELECT * FROM users WHERE name LIKE '%$q%'";
 } else if (isset($_GET["p"])) {
     $p = $_GET["p"];
     $perPage = 25;
     $startItem = ($p - 1) * $perPage;
-    $totalPage=ceil($userCount/$perPage);
+    $totalPage = ceil($userCount / $perPage);
     $sql = "SELECT * FROM users WHERE valid=1 LIMIT $startItem,$perPage";
 } else {
     header("location:pdo-users.php?p=1");
@@ -23,22 +23,17 @@ if (isset($_GET["q"])) {
 
 
 
-
 $stmt = $db_host->prepare($sql);
 
 try {
     $stmt->execute();
-    // while ($row = $stmt->fetch()) {
-    // 	echo "接收到的資料：<pre>";
-    // 	print_r($row);
-    // 	echo "</pre>";
-    // }
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // $userCount = $stmt->rowCount();
-
-    echo "<pre>";
-    // print_r($rows);
-    echo "</pre>";
+    if (isset($_GET["q"])) {
+        $userCount = $stmt->rowCount();
+    }
+    // echo "<pre>"; 會將結果展開，自動換行
+    // print_r($rows);會將結果用陣列的方式顯現出來
+    // echo "</pre>";
 } catch (PDOException $e) {
     echo "預處理陳述式執行失敗！ <br/>";
     echo "Error: " . $e->getMessage() . "<br/>";
@@ -67,13 +62,42 @@ $db_host = NULL;
         <div class="py-2">
             <a class="btn btn-primary" href="create-user.php"><i class="fa-solid fa-user-plus fa-fw"></i> Add User</a>
         </div>
-        <div class="py-2">
-            共 <?= $userCount ?> 位使用者
+        <div class="py-2 row g-3 align-items-center">
+            <div class="col-md-6">
+                <div class="hstack gap-2 align-items-center">
+                <?php if(isset($_GET["q"])):?>    
+                <a href="pdo-users.php" class="btn btn-primary"><i class="fa-solid fa-left-long fa-fw"></i></a>
+                <?php endif;?>
+                    <div>共 <?= $userCount ?> 位使用者
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+
+                <form action="" method="get">
+                    <div class="input-group">
+                        <input type="search" class="form-control" name="q"
+                            <?php
+                            // $q=""; if (isset($_GET["q"])) {
+                            // $q = $_GET["q"];
+                            // }
+                            // $q=(isset($_GET["q"]))?
+                            // $_GET["q"] : "";
+                            $q = $_GET["q"] ?? "";
+                            ?>
+                            value="<?= $q ?>">
+                        <button class="btn btn-primary">
+                            <i class="fa-solid fa-magnifying-glass fa-fw" type="submit"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php if ($userCount > 0): ?>
             <table class="table table-bordered">
                 <thead>
                     <tr>
+                        <th>id</th>
                         <th>name</th>
                         <th>account</th>
                         <th>phone</th>
@@ -86,6 +110,7 @@ $db_host = NULL;
                 <tbody>
                     <?php foreach ($rows as $row): ?>
                         <tr>
+                            <td><?= $row["id"] ?></td>
                             <td><?= $row["name"] ?></td>
                             <td><?= $row["account"] ?></td>
                             <td><?= $row["phone"] ?></td>
@@ -101,17 +126,21 @@ $db_host = NULL;
                     <?php endforeach; ?>
                 </tbody>
             </table>
-            <?php if(isset($_GET["p"])): ?>
-            <div>
-                <nav aria-label="">
-                    <ul class="pagination">
-                        <?php for($i=1;$i<=$totalPage;$i++):?>
-                        <li class="page-item"><a class="page-link" href="pdo-users.php?p=<?=$i?>"><?=$i?></a></li>
-                        <?php endfor; ?>
-                    </ul>
-                </nav>
-            </div>
-            <?php endif;?>
+            <?php if (isset($_GET["p"])): ?>
+                <div>
+                    <nav aria-label="">
+                        <ul class="pagination">
+                            <?php for ($i = 1; $i <= $totalPage; $i++): ?>
+                                <?php 
+                                    $active=($i==$_GET["p"])?
+                                    "active":"";
+                                    ?>
+                                <li class="page-item <?=$active?>"><a class="page-link" href="pdo-users.php?p=<?= $i ?>"><?= $i ?></a></li>
+                            <?php endfor; ?>
+                        </ul>
+                    </nav>
+                </div>
+            <?php endif; ?>
         <?php endif; ?>
     </div>
     <?php include("../daiski/js.php") ?>
---
+++ users/sign-up.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Sign Up</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <?php include("../daiski/css.php") ?>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6">
+                <div class="mt-3">
+                    <form action="doSignUp.php" method="post">
+                        <div class="mb-2">
+                            <label for="" class="form-label">帳號</label>
+                            <input type="text" class="form-control" name="account"
+                            required
+                            minlength="4" maxlength="20"
+                            >
+                            <div class="form-text">請輸入4~20字元的帳號</div>
+                        </div>
+                        <div class="mb-2">
+                            <label for="" class="form-label">密碼</label>
+                            <input type="password" class="form-control" name="password" required
+                            minlength="4" maxlength="20"
+                            >
+                            <div class="form-text">請輸入4~20字元的密碼</div>
+                        </div>
+                        <div class="mb-2">
+                            <label for="" class="form-label">確認密碼</label>
+                            <input type="password" class="form-control" name="repassword" required
+                            minlength="4" maxlength="20"
+                            >
+                        </div>
+                        <button class="btn btn-primary" type="submit">送出</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
